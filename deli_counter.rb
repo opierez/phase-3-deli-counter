be14a2245d8e8c2a493ffcd688d09edac5410dd8
olivia <opierez@gmail.com>
2023-01-08 20:22:49 -0500

    commit tree 12fdb4143b64febb43f69b52fcb71da95dfbb28f

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+katz_deli = []
+
+# [1, 2, 3, 4, 5, 6, 7]
+
+def line arr
+    if deli.empty?
+        puts "The line is currently empty."
+    else 
+        current_line = "The line is currently:"
+        arr.each.with_index do |person, index|
+            current_line << "#{index}. #{person}"
+        end
+        puts current_line
+    end
+end
